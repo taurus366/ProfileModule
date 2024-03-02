03d30bd0ff29eec27b490e5d76a12462d85773de
taurus366 <47596673+taurus366@users.noreply.github.com>
2024-03-02 19:48:36 +0200

    Add files via upload
    .

---
+++ src/test/java/com/profilemodule/www/ProfileModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profilemodule.www;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
