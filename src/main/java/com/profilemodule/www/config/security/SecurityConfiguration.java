9ccd9d7a575ae882b67c847c61515f66fd1232cc
Arcerr <taurus..ali47>
2024-04-03 23:14:31 +0300

    .

--- src/main/java/com/profilemodule/www/config/security/SecurityConfiguration.java
+++ src/main/java/com/profilemodule/www/config/security/SecurityConfiguration.java
@@ -2,9 +2,13 @@ package com.profilemodule.www.config.security;
 
 import com.vaadin.flow.spring.security.VaadinWebSecurity;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
@@ -13,32 +17,33 @@ public class SecurityConfiguration extends VaadinWebSecurity {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.authorizeHttpRequests((requests) -> requests
+        http
+                .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(new AntPathRequestMatcher("/images/*.png")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/line-awesome/**/*.svg")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
                         .requestMatchers(new AntPathRequestMatcher("/home")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/fonts/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
-                .requestMatchers(new AntPathRequestMatcher("/videos/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/css/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/fonts/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/images/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/js/**")).permitAll()
+                        .requestMatchers(new AntPathRequestMatcher("/videos/**")).permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/**").permitAll()
         );
+
+//        http
+//                .csrf(AbstractHttpConfigurer::disable); // Disable CSRF protection
+        http
+                .csrf(httpSecurityCsrfConfigurer -> httpSecurityCsrfConfigurer.ignoringRequestMatchers("/api/**"));
+
         http.formLogin((form) -> form
                 .loginPage("/login")
                 .defaultSuccessUrl("/user", true)
                 .permitAll()
         );
 
-//        http.logout((logout) -> logout
-//                .logoutUrl("/logout")
-//                .logoutSuccessUrl("/login")
-//                .invalidateHttpSession(true)
-//                .deleteCookies("JSESSIONID")
-//        );
-
         super.configure(http);
         setLoginView(http, "/login");
+
     }
 }
